8ec04abaf83dfb3255cb998320e5313f478a2803
brijesh <ahiranimau@gmail.com>
2025-03-10 10:28:04 +0530

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { useLocation } from "react-router-dom"; // Import useLocation
+import { useState, useEffect } from "react";
+import { useLocation,useNavigate } from "react-router-dom"; // Import useLocation
 import Sidebar from "./Components/Sidebar/Sidebar";
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
@@ -9,6 +9,7 @@ import "./App.css";
 function App() {
   const [sidebarData, setSidebarData] = useState("");
   const location = useLocation(); // Get the current route
+  const navigate = useNavigate();
 
   const handleSidebarDataChange = (data) => {
     setSidebarData(data);
@@ -16,12 +17,25 @@ function App() {
 
   //const [sessionExpired, setSessionExpired] = useState(false);
   //const storedUserString = localStorage.getItem("usertype");
-  //const token = localStorage.getItem("token");
+  const token = localStorage.getItem("token");
   //console.log(token);
   //console.log(storedUserString);
 
   // Check if the current route is "/login"
   const isLoginPage = location.pathname === "/";
+
+  // Redirect to login if no token is found (except on login page)
+  useEffect(() => {
+    if (!token && !isLoginPage) {
+      navigate("/");
+      //window.location.replace("/");
+    }
+  }, [token, isLoginPage]);
+
+  // Prevent rendering unauthorized pages before redirect
+  if (!token && !isLoginPage) {
+    return null; // Prevents unwanted page flashes
+  }
 
   return (
     <>
